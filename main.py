83ad803db9595fec9e26b27520b426afed25c3b7
kath <katya.permyakova.2004@mail.ru>
2022-02-04 18:22:43 +0300

    Большая задача по Maps API. Часть №2

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+import sys
+from PyQt5 import uic
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import Qt
+import maps
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+file_map = 'map.png'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('on.ui', self)
+
+        self.show_btn.clicked.connect(self.show_map)
+        self.scale.textChanged.connect(self.show_map)
+
+    def show_map(self):
+        place = self.lineEdit.text()
+        coods = maps.geocode_maps(place)
+        im_bit = maps.static_api(coods, self.scale.value())
+        with open(file_map, 'wb') as f:
+            f.write(im_bit)
+
+        self.pixmap = QPixmap(file_map)
+        self.label.setPixmap(self.pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            self.scale.setValue(self.scale.value() + 1)
+            self.show_map()
+        if event.key() == Qt.Key_PageDown:
+            self.scale.setValue(self.scale.value() - 1)
+            self.show_map()
 
 
-# Press the green button in the gutter to run the script.
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    sys.excepthook = except_hook
+    app = QApplication(sys.argv)
+    ex = Window()
+    ex.show()
+    sys.exit(app.exec_())
---
+++ maps.py
@@ -0,0 +1,63 @@
+import requests
+import sys
+from pprint import pprint
+
+
+def geocode_maps(toponym_to_find):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": toponym_to_find,
+        "format": "json"}
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+    if not response:
+        print('error')
+        sys.exit()
+
+    json_response = response.json()
+    # pprint(json_response)
+    toponym = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+    toponym_coodrinates = toponym["Point"]["pos"].split()
+
+    return toponym_coodrinates
+
+
+def search_maps(address_ll):
+    search_api_server = "https://search-maps.yandex.ru/v1/"
+    api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+
+    search_params = {
+        "apikey": api_key,
+        "text": "аптека",
+        "lang": "ru_RU",
+        "ll": address_ll,
+        "type": "biz",
+        "results": 10
+    }
+
+    response = requests.get(search_api_server, params=search_params)
+    if not response:
+        print('error')
+        sys.exit()
+
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+    # pprint(json_response)
+
+    return json_response
+
+
+def static_api(coords, scale):
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    map_params = {
+        "ll": ','.join(coords),
+        "z": scale,
+        "l": "map",
+        # "pt": f"{org_point},pm2dgl~{address_ll},home"
+    }
+    response = requests.get(map_api_server, params=map_params)
+
+    return response.content
+
+
